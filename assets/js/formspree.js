7ffc4fea5ec62b1094d4fc6b84c788cca187439a
Charles Jenrick Doolittle <dev.charlesjenrickd@gmail.com>
2020-06-19 14:36:43 +0800

    Add necessary files via upload.

---
+++ assets/js/formspree.js
@@ -0,0 +1,35 @@
+function success(){
+
+}
+
+function error(){
+
+}
+
+$(document).ready(function(){
+    $('#fsForm').on('submit', function(e){
+        e.preventDefault();
+
+        $.ajax({
+            url: 'https://formspree.io/xoqkljkl',
+            method: 'POST',
+            headers: {
+                "Acceept" : "application/json"
+            },
+            data:{
+                name: $('#fsfName').val(),
+                _replyto: $('#fsfEmail').val(),
+                message: $('#fsfMessage').val()
+            }
+        })
+        .done(function(data){
+            $('#fsfAlert').removeClass('hidden').addClass('alert-success')
+            $('#fsfAlert .form-message').html('Your message has been successfully sent. Thanks!')
+        })
+        .fail(function(error){
+            $('#fsfAlert').removeClass('hidden').addClass('alert-danger')
+            $('#fsfAlert .form-message').html('An error occured while sending your message. Please try again.')
+        })
+
+    });
+});
